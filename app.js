3c75d23c8e6472dea3ff11d33ff4c407d65c3925
bpeterson1663 <bpeterson1663@gmail.com>
2016-02-29 15:04:10 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $('#generator').on('click',appendDom);
+  $('.container').on('click','button',removeFromDom);
+});
+
+function appendDom(){
+  $('.container').append('<ul class="propertyRow"></ul>');
+  var $el = $('.container').children().last();
+
+  var propertyId = randomNumber(100,10000);
+  var squareFoot = randomNumber(1000,10000);
+  var valueSquareFoot = randomNumber(1,50);
+  //el.append('<p>'+ propertyId + '</p>');
+
+  $el.append('<li>Property ID: ' + propertyId + '</li>');
+  $el.append('<li>Square Foot: ' + squareFoot + '</li>');
+  $el.append('<li>$' + valueSquareFoot + ' sq/ft</li>');
+  $el.append('<button>Delete</button>');
+  $el.hide().slideDown();
+
+};
+
+function randomNumber(min, max){
+  return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function removeFromDom(){
+  $(this).parent().slideUp();
+
+}
